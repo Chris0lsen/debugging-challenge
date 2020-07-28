4b345de6d9b4edc4f8aa7eb9aa6b8d9e604912cc
chris0lsen <chris.olsen@kuali.co>
2020-07-28 15:27:39 -0500

    added sqlite and updated route to use it

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
 const apiRoutes = require('./routes/api')
+const databaseMiddleware = require('./database')
 
 const PORT = 3000
+
+
 
 app.use('/build', express.static('client/build'))
 app.use('/static', express.static('client/static'))
@@ -12,7 +14,7 @@ app.use('/static', express.static('client/static'))
 app.get('/', (_, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
-
+app.use(databaseMiddleware())
 app.use('/api', apiRoutes)
 
 app.listen(PORT)
